018f6bed3ec15f3545070ce98b5add61220637ea
Ho1ocron <cmrfreeze@gmail.com>
2025-11-20 14:46:54 +0500

    added weather getter

--- forecast/data/models.py
+++ forecast/data/models.py
@@ -1,2 +1,11 @@
+from datetime import datetime
+
+
 class Weather:
-    ...
+    interval: int
+    is_day: int
+    temperature: float
+    time: datetime
+    weathercode: str # Wether code will be translated in string name instead of code
+    winddirection: float
+    windspeed: float
--- forecast/data/requests.py
+++ forecast/data/requests.py
@@ -1,22 +1,44 @@
-import httpx
-import asyncio
+from httpx import AsyncClient
+from asyncio import run, gather
+from pprint import pprint
+
+from processor import get_citys_coords
+from models import Weather
 
 
-async def fetch_url(url, client=None):
-    """Fetch a single URL asynchronously"""
-    if client is None:
-        client = httpx.AsyncClient()
+async def get_weather(
+    name: str= "Yekaterinburg", 
+    url: str = "https://api.open-meteo.com/v1/forecast"
+) -> Weather | None:
+    lantitude, longitude = await get_citys_coords("Moscow")
+    params = {
+        "latitude": lantitude,
+        "longitude": longitude,
+        "current_weather": True
+    }
+    async with AsyncClient() as client:
+        response = await client.get(url, params=params)
+        weather = response.json()
+        pprint(weather["current_weather"])
 
-    response = await client.get(url, timeout=5)
 
-    if response.status_code != 200:
-        return None
+if __name__ == "__main__": 
+    run(get_weather())
+# async def fetch_url(url, client=None):
+#     """Fetch a single URL asynchronously"""
+#     if client is None:
+#         client = httpx.AsyncClient()
 
-    return response.json()
+#     response = await client.get(url, timeout=5)
+
+#     if response.status_code != 200:
+#         return None
+
+#     return response.json()
 
 
-async def fetch_multiple_urls(urls):
-    """Fetch multiple URLs concurrently"""
-    async with httpx.AsyncClient() as client:
-        tasks = [fetch_url(url, client) for url in urls]
-        return [x for x in await asyncio.gather(*tasks) if x]
+# async def fetch_multiple_urls(urls):
+#     """Fetch multiple URLs concurrently"""
+#     async with httpx.AsyncClient() as client:
+#         tasks = [fetch_url(url, client) for url in urls]
+#         return [x for x in await asyncio.gather(*tasks) if x]
