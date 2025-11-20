88fac37cd19dc743aede941659201750b95cb648
Ho1ocron <cmrfreeze@gmail.com>
2025-11-20 15:22:12 +0500

    cleared the code

--- forecast/data/requests.py
+++ forecast/data/requests.py
@@ -53,12 +53,14 @@ async def serializer(key: str, value: str) -> datetime | float | str:
 
 
 async def get_weather(
-    name: str= "Yekaterinburg", 
+    name: str = "Yekaterinburg",
     url: str = "https://api.open-meteo.com/v1/forecast"
 ) -> Weather | None:
     if name == "":
         return None
+
     lantitude, longitude = await get_citys_coords(name)
+
     params = {
         "latitude": lantitude,
         "longitude": longitude,
@@ -69,31 +71,11 @@ async def get_weather(
         response = await client.get(url, params=params)
         if response.status_code != 200:
             return None
-        
+
         forecast: dict[str, dict[str, str]] = response.json()
         weather = Weather(*[await serializer(key, value) for key, value in forecast["current_weather"].items()])
         return weather
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     print(run(get_weather()))
-
-    
-# async def fetch_url(url, client=None):
-#     """Fetch a single URL asynchronously"""
-#     if client is None:
-#         client = httpx.AsyncClient()
-
-#     response = await client.get(url, timeout=5)
-
-#     if response.status_code != 200:
-#         return None
-
-#     return response.json()
-
-
-# async def fetch_multiple_urls(urls):
-#     """Fetch multiple URLs concurrently"""
-#     async with httpx.AsyncClient() as client:
-#         tasks = [fetch_url(url, client) for url in urls]
-#         return [x for x in await asyncio.gather(*tasks) if x]
